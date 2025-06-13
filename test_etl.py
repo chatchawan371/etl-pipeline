d3f97672a7c9293ba63c6add10296dc23e16e086
chatchawan371 <139434016+chatchawan371@users.noreply.github.com>
2025-06-13 13:38:36 +0700

    Late.130620251338

--- test_etl.py
+++ test_etl.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 dfInput = pd.read_csv('sample_products.csv')
-dfOutput = pd.read_csv('output/clean_products.csv')
+dfOutput = pd.read_csv('output/cleaned_products.csv')
 
 with open('output/test_result.txt', 'a') as f:
     f.write(datetime.now().strftime("%d-%m-%Y %H:%M:%S") + '\n')
